057e42f64f77697a29b79e7e7e7314251ff28d6e
3363 <sylwia.codes@yandex.com>
2017-02-09 23:23:36 -0800

    Add 3 problems from IPND Front-End extra challanges.

---
+++ alphabetizer.js
@@ -0,0 +1,58 @@
+/***
+Twelve people have walked on the Moon. They are:
+
+Neil Armstrong
+Buzz Aldrin
+Pete Conrad
+Alan Bean
+Alan Shepard
+Edgar Mitchell
+David Scott
+James Irwin
+John W Young
+Charles Duke
+Eugene Cernan
+Harrison Schmitt
+You'll notice that these names are listed in the order that they first stepped on the Moon, not alphabetical order. To alphabetize them, it would make sense to write their names as lastname, firstname. For example: Neil Armstrong becomes Armstrong, Neil.
+
+Finish the alphabetizer(_names) function, which takes in a names array (of length N) containing strings of names and returns an alphabetized array of names in lastname, firstname format.
+***/
+var moonWalkers = [
+  "Neil Armstrong",
+  "Buzz Aldrin",
+  "Pete Conrad",
+  "Alan Bean",
+  "Alan Shepard",
+  "Edgar Mitchell",
+  "David Scott",
+  "James Irwin",
+  "John Young",
+  "Charles Duke",
+  "Eugene Cernan",
+  "Harrison Schmitt"
+];
+
+function nameSwitch(string) {
+  var stringArray = string.split(" ");
+  //console.log(stringArray);
+  var newString = stringArray[1]  + ", " + stringArray[0];
+  return newString;
+}
+
+
+function alphabetizer(names) {
+  var newNames = [];
+
+  for(var i = 0; i < names.length; i++) {
+    var newName = nameSwitch(names[i]);
+    newNames.push(newName);
+  }
+
+  newNames.sort();
+
+  return newNames;
+
+}
+
+// Try logging your results to test your code!
+console.log(alphabetizer(moonWalkers));
